ec8f1000894c0321d0f6507a8a019782c0b3a323
pragadeesh-vakilsearch <pragadeesh@vakilsearch.com>
2016-01-23 20:56:54 +0530

    add restaurant resource

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'bootstrap-sass'
 group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'spring'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  get 'pages/about'
+  root to: 'restaurants#index'
+  get 'pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
